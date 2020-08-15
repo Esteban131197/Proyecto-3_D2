5f71e2924ea677e01368cd022e5b61655bca3a86
Esteban131197 <eamu11@hotmail.com>
2020-08-14 23:48:21 -0600

    Cambios al mysql

--- restapi/src/usuarios.js
+++ restapi/src/usuarios.js
@@ -41,12 +41,12 @@ router.delete('/:id', (req, res) => {
 // INSERT An usser
 
 router.post('/', (req, res) => {
-    const {id, name, repositorio, file, ultimo_cambio} = req.body;
-    console.log(id, name, repositorio, file, ultimo_cambio);
+    const {id, name, repositorio, commitdescripcion} = req.body;
+    console.log(id, name, repositorio, commitdescripcion);
     const query = `
-      CALL usuariosAddOrEdit(?, ?, ?, ?, ?);
+      CALL usuariosAddOrEdit(?, ?, ?, ?);
     `;
-    mysqlConnection.query(query, [id, name, repositorio, file, ultimo_cambio], (err, rows, fields) => {
+    mysqlConnection.query(query, [id, name, repositorio, commitdescripcion], (err, rows, fields) => {
       if(!err) {
         res.json({status: 'Usser Saved'});
       } else {
@@ -57,17 +57,16 @@ router.post('/', (req, res) => {
   });
   
  router.put('/:id', (req, res) => {
-    const {name, repositorio, file, ultimo_cambio } = req.body;
+    const {name, repositorio, commitdescripcion} = req.body;
     const { id } = req.params;
     const query = `
       SET @id = ?;
       SET @name = ?;
       SET @repositorio = ?;
-      SET @file = ?;
-      SET @ultimo_cambio = ?;
-      CALL usuariosAddOrEdit(@id, @name, @repositorio, @file, @ultimo_cambio);
+      SET @commitdescripcion;
+      CALL usuariosAddOrEdit(@id, @name, @repositorio, @commitdescripcion);
     `;
-    mysqlConnection.query(query, [id, name, repositorio, file, ultimo_cambio], (err, rows, fields) => {
+    mysqlConnection.query(query, [id, name, repositorio, commitdescripcion], (err, rows, fields) => {
       if(!err) {
         res.json({status: 'Usser Updated'});
       } else {
