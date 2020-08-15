25f162fca1303dacf320186245733036a424bf0a
Esteban131197 <eamu11@hotmail.com>
2020-08-15 09:00:14 -0600

    Cambios a cliente

--- client/Got/Got/LoginForm.h
+++ client/Got/Got/LoginForm.h
@@ -9,6 +9,7 @@ namespace Got {
 	using namespace System::Data;
 	using namespace System::Drawing;
 	using namespace MySql::Data::MySqlClient;
+	using namespace System::IO;
 	/// <summary>
 	/// Summary for LoginForm
 	/// </summary>
@@ -45,6 +46,13 @@ namespace Got {
 	private: System::Windows::Forms::RichTextBox^ DCBox;
 	private: System::Windows::Forms::Button^ CommitBoton;
 	private: System::Windows::Forms::NumericUpDown^ IDStock;
+	private: System::Windows::Forms::Button^ AABoton;
+	private: System::Windows::Forms::PictureBox^ Imagen;
+
+
+
+
+	private: System::ComponentModel::IContainer^ components;
 
 
 		   //private: System::Windows::Forms::Label^ lblName;
@@ -59,7 +67,7 @@ namespace Got {
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container^ components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -77,7 +85,10 @@ namespace Got {
 			this->DCBox = (gcnew System::Windows::Forms::RichTextBox());
 			this->CommitBoton = (gcnew System::Windows::Forms::Button());
 			this->IDStock = (gcnew System::Windows::Forms::NumericUpDown());
+			this->AABoton = (gcnew System::Windows::Forms::Button());
+			this->Imagen = (gcnew System::Windows::Forms::PictureBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->IDStock))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Imagen))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// IDLabel
@@ -140,7 +151,7 @@ namespace Got {
 			// 
 			// CommitBoton
 			// 
-			this->CommitBoton->Location = System::Drawing::Point(168, 172);
+			this->CommitBoton->Location = System::Drawing::Point(160, 169);
 			this->CommitBoton->Name = L"CommitBoton";
 			this->CommitBoton->Size = System::Drawing::Size(135, 39);
 			this->CommitBoton->TabIndex = 8;
@@ -155,11 +166,32 @@ namespace Got {
 			this->IDStock->Size = System::Drawing::Size(60, 20);
 			this->IDStock->TabIndex = 9;
 			// 
+			// AABoton
+			// 
+			this->AABoton->Location = System::Drawing::Point(430, 152);
+			this->AABoton->Name = L"AABoton";
+			this->AABoton->Size = System::Drawing::Size(113, 56);
+			this->AABoton->TabIndex = 10;
+			this->AABoton->Text = L"Agregar Archivo";
+			this->AABoton->UseVisualStyleBackColor = true;
+			this->AABoton->Click += gcnew System::EventHandler(this, &LoginForm::AABoton_Click);
+			// 
+			// Imagen
+			// 
+			this->Imagen->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->Imagen->Location = System::Drawing::Point(430, 19);
+			this->Imagen->Name = L"Imagen";
+			this->Imagen->Size = System::Drawing::Size(113, 96);
+			this->Imagen->TabIndex = 11;
+			this->Imagen->TabStop = false;
+			// 
 			// LoginForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(377, 244);
+			this->ClientSize = System::Drawing::Size(595, 244);
+			this->Controls->Add(this->Imagen);
+			this->Controls->Add(this->AABoton);
 			this->Controls->Add(this->IDStock);
 			this->Controls->Add(this->CommitBoton);
 			this->Controls->Add(this->DCBox);
@@ -173,6 +205,7 @@ namespace Got {
 			this->Text = L"Acceso";
 			this->Load += gcnew System::EventHandler(this, &LoginForm::LoginForm_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->IDStock))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Imagen))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -191,7 +224,7 @@ namespace Got {
 		//Port = 3306  
 		//Database Name = got
 		//UID = root
-		//Password = admin 
+		//Password = admin
 
 
 
@@ -216,6 +249,16 @@ namespace Got {
 		String^ NombreRepo = NRBox->Text;
 		String^ DescripCommit = DCBox->Text;
 		int Id = Convert::ToInt32(IDStock->Value);
+		array<unsigned char>^ Archivo;
+
+
+		String^ fileLocation = Imagen->ImageLocation;
+		FileStream^ fs = gcnew FileStream(fileLocation, FileMode::Open, FileAccess::Read);
+		BinaryReader^ br = gcnew BinaryReader(fs);
+		Archivo = br->ReadBytes(fs->Length);
+
+
+
 
 		bool isValid = true;
 
@@ -238,7 +281,7 @@ namespace Got {
 
 			conn->Open();
 
-			String^ cmdString = "INSERT INTO usuarios (id,name,repositorio,commitdescripcion) VALUES (@Id,@Nombre,@NombreRepo,@DescripCommit)";
+			String^ cmdString = "INSERT INTO usuarios (id,name,repositorio,commitdescripcion,visualizaciondocumento) VALUES (@Id,@Nombre,@NombreRepo,@DescripCommit,@Archivo)";
 
 
 			MySqlCommand^ cmd = gcnew MySqlCommand(cmdString, conn);
@@ -248,11 +291,14 @@ namespace Got {
 			cmd->Parameters->AddWithValue("@Nombre", Nombre);
 			cmd->Parameters->AddWithValue("@NombreRepo", NombreRepo);
 			cmd->Parameters->AddWithValue("@DescripCommit", DescripCommit);
+			cmd->Parameters->AddWithValue("@Archivo", Archivo);
+
 
 
 			try {
 				cmd->ExecuteNonQuery();
 				MessageBox::Show("Commit ingresado Correctamente");
+				conn->Close();
 			}
 			catch (Exception^ e) {
 				MessageBox::Show("Error al insertar datos");
@@ -265,6 +311,21 @@ namespace Got {
 
 		}
 	}
+	private: System::Void AABoton_Click(System::Object^ sender, System::EventArgs^ e) {
+
+		OpenFileDialog^ ofd = gcnew OpenFileDialog();
+		ofd->Filter = "TXT Files *.txt|*.txt|PY Files *.py|*.py|CS Files *.cs|*.cs|JS Files *.js|*.js|CPP Files *.cpp|*.cpp|All Files *.*|*.*";
+
+		if (ofd->ShowDialog() == Windows::Forms::DialogResult::OK); {
+			Imagen->ImageLocation = ofd->FileName;
+
+		}
+
+
+
+
+	}
 	};
 }
+
 
--- restapi/mysql/db.sql
+++ restapi/mysql/db.sql
@@ -6,20 +6,12 @@ CREATE TABLE usuarios (
 	id INT (11) NOT NULL AUTO_INCREMENT,
     name VARCHAR(45) DEFAULT NULL,
     repositorio VARCHAR(45) DEFAULT NULL,
-    branch VARCHAR(45) DEFAULT NULL,
-    files INT (11) DEFAULT NULL,
-    ultimo_cambio INT (11) DEFAULT NULL, 
+    commitdescripcion VARCHAR(45) DEFAULT NULL,
+    visualizaciondocumento longblob,
     
     PRIMARY  KEY(id)
 );
 
-DESCRIBE usuarios;
-
-INSERT INTO usuarios values
-	(1, 'Esteban', 'Proy1', 'Master', 0, 9),
-    (2, 'Brayan', 'Proy2', 'Master', 1, 7);
-	
-    
 SELECT * FROM usuarios;
 
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'admin';
